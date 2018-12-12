e377c918b3f2ab4d0b1ad93148dfc60571d1ae35
Xiaoming (Jason) Cui <xiaoming.cui@intel.com>
2018-12-11 16:16:43 -0800

    Modified the code, and it can generate batched iterator data

--- iterator/testinferenceiterator.py
+++ iterator/testinferenceiterator.py
@@ -4,41 +4,72 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import codecs
+import argparse
+import sys
+
+import numpy as np
+from google.protobuf import text_format
+
 import tensorflow as tf
-
 from tensorflow.python.ops import lookup_ops
-
 import iterator_utils
+import vocab_utils
 
+def load_data(inference_input_file):
+  """Load inference data."""
+  with codecs.getreader("utf-8")(
+      tf.gfile.GFile(inference_input_file, mode="rb")) as f:
+    inference_data = f.read().splitlines()
 
+  return inference_data
 
-
-
-src_vocab_table = lookup_ops.index_table_from_tensor(
-    tf.constant(["a", "b", "c", "eos", "sos"]))
-src_dataset = tf.data.Dataset.from_tensor_slices(
-    tf.constant(["c c a", "c a", "d", "f e a g"]))
 hparams = tf.contrib.training.HParams(
     random_seed=3,
     eos="eos",
-    sos="sos")
-batch_size = 2
-src_max_len = 3
+    sos="sos",
+    src_vocab_file="/mnt/nrvlab_300G_work01/cuixiaom/Data/Nmt/Ger-Eng/Training/wmt16/vocab.bpe.32000.de",
+    tgt_vocab_file="/mnt/nrvlab_300G_work01/cuixiaom/Data/Nmt/Ger-Eng/Training/wmt16/vocab.bpe.32000.en",
+    share_vocab=False,
+    src_max_len_infer=50,
+    use_char_encode=False)
+
+src_vocab_file = hparams.src_vocab_file
+tgt_vocab_file = hparams.tgt_vocab_file
+
+src_vocab_table, tgt_vocab_table = vocab_utils.create_vocab_tables(
+    src_vocab_file, tgt_vocab_file, hparams.share_vocab)
+reverse_tgt_vocab_table = lookup_ops.index_to_string_table_from_file(
+    tgt_vocab_file, default_value=vocab_utils.UNK)
+
+src_placeholder = tf.placeholder(shape=[None], dtype=tf.string)
+batch_size_placeholder = tf.placeholder(shape=[], dtype=tf.int64)
+
+src_dataset = tf.data.Dataset.from_tensor_slices(
+    src_placeholder)
 iterator = iterator_utils.get_infer_iterator(
-    src_dataset=src_dataset,
-    src_vocab_table=src_vocab_table,
-    batch_size=batch_size,
-    eos=hparams.eos,
-    src_max_len=src_max_len)
+        src_dataset,
+        src_vocab_table,
+        batch_size=batch_size_placeholder,
+        eos=hparams.eos,
+        src_max_len=hparams.src_max_len_infer,
+        use_char_encode=hparams.use_char_encode)
+
 table_initializer = tf.tables_initializer()
 source = iterator.source
 seq_len = iterator.source_sequence_length
-#assertEqual([None, None], source.shape.as_list())
-#assertEqual([None], seq_len.shape.as_list())
+inference_input_file="newstest2014.tok.bpe.32000_100w.de"
+infer_data = load_data(inference_input_file)
+batch_size = 1
+
 with tf.Session() as sess:
   sess.run(table_initializer)
-  sess.run(iterator.initializer)
-
+  sess.run(
+    iterator.initializer,
+    feed_dict={
+        src_placeholder: infer_data,
+        batch_size_placeholder: batch_size
+    })
   while  True:
     try:
       print("a new batch!")
